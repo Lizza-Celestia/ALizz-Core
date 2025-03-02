79141a047f5b36f3cb7e53a9b05a13b67a72ce66
Lizza Celestia <lizza.celestia+github@gmail.com>
2025-03-02 20:39:57 +0100

    Update base_plugin.py
    Version 1.0 update

--- core/base_plugin.py
+++ core/base_plugin.py
@@ -1,9 +1,9 @@
 """
 File Location: core/base_plugin.py
 Author: Lizza Celestia
-Version: ALizz_AI_V0_9
+Version: ALizz_AI_V1_0
 Create Date: 2025-02-12
-Modified Date: 2025-02-14
+Modified Date: 2025-03-02
 Description:
 """
 from abc import ABC
@@ -13,15 +13,31 @@ class BasePlugin(ABC):
 #####################################################
 #                  Initialize
 #####################################################
-    def __init__(self, signals, enabled=True):
-        self.signals = signals
-        self.enabled = enabled
-
+    def __init__(self, core):
+        self.core = core
+        self.status = False
+        self.loop = None  # Store the loop reference
+        self.stop_event = asyncio.Event()  # Async event to signal stopping
+        self.subscriptions_list = {}  # Subscription list for the plugin
+        
+        # Subscribe to the event in subscriptions_list
+        for event, handler in self.subscriptions_list.items():
+            self.core.event_bus.subscribe(event, getattr(self, handler))
+            
 #####################################################
 #                  Tread loop
 #####################################################
     def init_event_loop(self):
-        asyncio.run(self.run())
+        """Creates and runs a new event loop inside a thread."""
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+        try:
+            self.loop.run_until_complete(self.run())  # Start main async function
+        finally:
+            self.status = False
+            if not self.loop.is_closed():
+                self.loop.close()  # Ensure loop is properly closed
 
 #####################################################
 #                  Run sequence
