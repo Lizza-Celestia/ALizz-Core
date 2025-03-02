f4b207faa3f68b998be52ee85dfc59f9a08e823b
Lizza Celestia <lizza.celestia+github@gmail.com>
2025-03-02 20:45:26 +0100

    Update core.py
    Version 1.0 update

--- core/core.py
+++ core/core.py
@@ -1,35 +1,30 @@
 """
 File Location: core/core.py
 Author: Lizza Celestia
-Version: ALizz_AI_V0_9
+Version: ALizz_AI_V1_0
 Create Date: 2025-02-12
-Modified Date: 2025-02-13
+Modified Date: 2025-03-02
 Description:
 """
 # core/core.py
 
 from core.plugin_manager import PluginManager
 from core.event_bus import EventBus
-import sys
+# import sys
 import threading
 import logging
 logger = logging.getLogger(__name__)
-
-# Path to the JSON file
-plugin_setting_path = "settings_plugins.json"
-
 
 class Core:
     """
     The core manages the system’s overall lifecycle and
     makes the event bus and plugin manager available to all plugins.
     """
-    def __init__(self, plugin_directory: str):
+    def __init__(self, plugin_directory: str, priority_plugins):
         self.event_bus = EventBus()
         # Pass 'self' into the PluginManager so it can access the core/event bus
-        self.plugin_manager = PluginManager(plugin_directory, core=self)
+        self.plugin_manager = PluginManager(plugin_directory, core=self, priority_plugins=priority_plugins)
         self.module_threads = {}
-
 
     def boot(self):
         """
@@ -38,10 +33,11 @@ class Core:
         logger.info("Core booting up...")
         self.plugin_manager.discover_plugins()
         logger.info("All discovered plugins have been started.")
+        logger.debug(f"Loaded Plugin successfully: {self.plugin_manager.loaded_plugins}")
 
         logger.debug(f"Start 'Event bus' thread")
         try:
-            event_thread = threading.Thread(target=self.event_bus.process_events, daemon=True)
+            event_thread = threading.Thread(target=self.event_bus.process_events,name=f"Thread-event_bus", daemon=True)
             event_thread.start()
         except Exception as e:
             logger.error(f"Could not start the 'Event bus' thread. Error: {e}")
@@ -50,12 +46,6 @@ class Core:
         """
         Gracefully stop and unload all plugins.
         """
-        logger.debug("Publishing 'TERMINATE_PLUGINS' Event.")
-        self.event_bus.publish(
-            "TERMINATE_PLUGINS",
-            {"message": False}
-            )
-        
         logger.info("Core, shutting down all plugins...")
         for plugin_name in list(self.plugin_manager.loaded_plugins.keys()):
             self.plugin_manager.unload_plugin(plugin_name)
@@ -67,12 +57,3 @@ class Core:
             {"message": False}
             )
         
-        sys.exit(0)
-
-
-
-
-
-
-
-
